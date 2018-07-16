f3ac003919d7660843846d5da53e40a97bdce357
UJ\Блохин Евгений <evgenyblokhin@mail.ru>
2018-07-16 21:43:19 +0700

    16.07.18

--- app/src/main/java/ru/uj/beatbox/SoundViewModel.java
+++ app/src/main/java/ru/uj/beatbox/SoundViewModel.java
@@ -27,4 +27,8 @@ public class SoundViewModel extends BaseObservable {
         mSound = sound;
         notifyChange();
     }
+
+    public void onButtonClicked() {
+        mBeatBox.play(mSound);
+    }
 }
